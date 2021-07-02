d2081e3d96722cb4a860a19bd2daa7efdb9f4925
Artur264 <85124020+Artur264@users.noreply.github.com>
2021-07-03 01:37:23 +0300

    add-js

---
+++ scripts/script.js
@@ -0,0 +1,14 @@
+let popUp = document.querySelector(".popup");
+let btn = document.querySelector('.btn-popup');
+let popupClose = document.querySelector('.btn-popup-close')
+
+function showPoUpOnClick() {
+    popUp.classList.add('popup-show');
+}
+
+btn.addEventListener("click", showPoUpOnClick);
+
+popupClose.addEventListener('click', function () {
+    popUp.classList.remove('popup-show')
+})
+
